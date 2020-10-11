2eed6486513f9235e5c7520e6cb58a5e0ddbfe7b
Gulyas Gergely <gulyas.gergely@hotmail.com>
2020-10-11 11:10:27 +0200

    RB-45: Optimized navbar for mobile

--- resources/views/display.blade.php
+++ resources/views/display.blade.php
@@ -1,43 +1,40 @@
 @extends ('layout')
 
 @section ('content')
-<nav class="navbar navbar-expand-lg fixed-top navbar-light bg-light">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
+<nav class="navbar navbar-expand-sm fixed-top navbar-light bg-light">
+    <!-- Tab selectors -->
+    <ul class="navbar-nav">
+        <div class="btn-group" role="group" aria-label="Sticky categories">
+            <a class="nav-item nav-link {{ Request::is('display/*/0') ? 'active text-success' : ''}} width-1 btn btn-light" id="nav-wentwell-tab" href="/display/{{$bid}}/0" role="tab" aria-controls="nav-wentwell" aria-selected="true" title="Went Well"><i class="fas fa-thumbs-up"></i></a>
+            <a class="nav-item nav-link {{ Request::is('display/*/2') ? 'active text-danger' : ''}} width-1 btn btn-light" id="nav-needsimprovement-tab" href="/display/{{$bid}}/2" role="tab" aria-controls="nav-needsimprovement" aria-selected="false" title="Needs Improvement"><i class="fas fa-thumbs-down"></i></a>
+            <a class="nav-item nav-link {{ Request::is('display/*/1') ? 'active text-warning' : ''}} width-1 btn btn-light" id="nav-actionitem-tab" href="/display/{{$bid}}/1" role="tab" aria-controls="nav-actionitem" aria-selected="false" title="Action Item"><i class="fas fa-exclamation"></i></a>
+        </div>
+    </ul>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <!-- Tab selectors -->
-        <ul class="navbar-nav">
-            <div class="btn-group" role="group" aria-label="Sticky categories">
-                <a class="nav-item nav-link {{ Request::is('display/*/0') ? 'active text-success' : ''}} width-1 btn btn-light" id="nav-wentwell-tab" href="/display/{{$bid}}/0" role="tab" aria-controls="nav-wentwell" aria-selected="true" title="Went Well"><i class="fas fa-thumbs-up"></i></a>
-                <a class="nav-item nav-link {{ Request::is('display/*/2') ? 'active text-danger' : ''}} width-1 btn btn-light" id="nav-needsimprovement-tab" href="/display/{{$bid}}/2" role="tab" aria-controls="nav-needsimprovement" aria-selected="false" title="Needs Improvement"><i class="fas fa-thumbs-down"></i></a>
-                <a class="nav-item nav-link {{ Request::is('display/*/1') ? 'active text-warning' : ''}} width-1 btn btn-light" id="nav-actionitem-tab" href="/display/{{$bid}}/1" role="tab" aria-controls="nav-actionitem" aria-selected="false" title="Action Item"><i class="fas fa-exclamation"></i></a>
-            </div>
-        </ul>
         <a class="navbar-brand mx-auto" href="/">Retro Board</a>
-        <!-- Div toggle -->
-        <form class="form-inline my-2 mr-2 my-lg-0">
-            <label for="hideStickyDiv" class="switch">
-                <input class="ml-2" type="checkbox" name="hideStickyDiv" id="hideStickyDiv" title="Toggle sticky visibility" {{ old('hideStickyDiv') }}>
-                <span class="slider round"></span>
-            </label>
-        </form>
-        <!-- Buttons -->
-        @if($protected)
-            <form class="form-inline my-2 mr-2 my-lg-0" method="GET" action="/lock">
-                <input type="hidden" name="bid" value="{{ $bid }}">
-                <button type="submit" class="btn btn-outline-warning btn-lock-board width-2" id="lockboard" title="Lock board"></button>
-            </form>
-        @endif
-        <ul class="nav">
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle text-secondary" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false"><i class="fas fa-ellipsis-v"></i></a>
-                <div class="dropdown-menu dropdown-menu-lg-right">
-                    <button type="button" class="btn btn-link text-danger" id="deleteall" title="Clear board" data-toggle="modal" data-target="#clearAllStickyModal" data-bid="{{ $bid }}"><i class="fas fa-quidditch mr-2"></i>Clear board</button>
-                </div>
-            </li>
-        </ul>
     </div>
+    <!-- Div toggle -->
+    <form class="form-inline my-2 mr-2 my-lg-0">
+        <label for="hideStickyDiv" class="switch">
+            <input class="ml-2" type="checkbox" name="hideStickyDiv" id="hideStickyDiv" title="Toggle sticky visibility" {{ old('hideStickyDiv') }}>
+            <span class="slider round"></span>
+        </label>
+    </form>
+    <!-- Buttons -->
+    @if($protected)
+        <form class="form-inline my-2 mr-2 my-lg-0" method="GET" action="/lock">
+            <input type="hidden" name="bid" value="{{ $bid }}">
+            <button type="submit" class="btn btn-outline-warning btn-lock-board width-2" id="lockboard" title="Lock board"></button>
+        </form>
+    @endif
+    <ul class="nav">
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle text-secondary" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false"><i class="fas fa-ellipsis-v"></i></a>
+            <div class="dropdown-menu dropdown-menu-lg-right">
+                <button type="button" class="btn btn-link text-danger" id="deleteall" title="Clear board" data-toggle="modal" data-target="#clearAllStickyModal" data-bid="{{ $bid }}"><i class="fas fa-quidditch mr-2"></i>Clear board</button>
+            </div>
+        </li>
+    </ul>
 </nav>
 
 <!-- Add Modal -->
