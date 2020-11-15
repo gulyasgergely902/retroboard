ca8d50d7f814ec5ec665691b10a3faf828a31e07
Gulyas Gergely <gulyas.gergely@hotmail.com>
2020-11-15 18:50:16 +0100

    RB-51: Create api for getting all, adding and removing boards, stickies and groups

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Board;
+use App\Sticky;
+use App\Group;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +18,40 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/boards', function(Request $request){
+    return Board::all();
+});
+
+Route::get('/boards/add', function(Request $request){
+    return "Add board";
+});
+
+Route::get('/boards/delete/{bid}', function(Request $request, $bid){
+    return "Delete board: $bid";
+});
+
+Route::get('/stickies', function(Request $request){
+    return Sticky::all();
+});
+
+Route::get('/stickies/add', function(Request $request){
+    return "Add sticky";
+});
+
+Route::get('/stickies/delete/{sid}', function(Request $request, $sid){
+    return "Delete sticky: $sid";
+});
+
+Route::get('/groups', function(Request $request){
+    return Group::all();
+});
+
+Route::get('/groups/add', function(Request $request){
+    return "Add group";
+});
+
+Route::get('/groups/delete/{gid}', function(Request $request, $gid){
+    return "Delete group: $gid";
 });
