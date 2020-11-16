f29c50c4e7a16aa1b54bdd26de25c2cb7e969648
Gulyas Gergely <gulyas.gergely@hotmail.com>
2020-11-16 15:00:11 +0100

    RB-51: Modified endpoints: made more simple, used get, post, delete methods instead of specific endpoint to the action.

--- routes/api.php
+++ routes/api.php
@@ -24,34 +24,67 @@ Route::get('/boards', function(Request $request){
     return Board::all();
 });
 
-Route::get('/boards/add', function(Request $request){
-    return "Add board";
+Route::post('/boards', function(Request $request){
+    $validateFormData = $request->validate([
+        'board_name' => 'required|max:60',
+        'board_password' => 'max:60'
+    ]);
+
+    $board = new Board();
+    $board->board_name = $request->input('board_name');
+    $board->secure = ($request->input('secure_board') == "on" ? 1 : 0);
+    $board->board_password = Hash::make($request->input('board_password'));
+    $board->save();
+    return response("New board added", 201)->header('Content-Type', 'text/plain');
 });
 
-Route::get('/boards/delete/{bid}', function(Request $request, $bid){
-    return "Delete board: $bid";
+Route::delete('/boards', function(Request $request){
+    $bid = $request->input('bid');
+    Sticky::where('bid', $bid)->delete();
+    Group::where('board_id', $bid)->delete();
+    Board::where('board_id', $bid)->delete();
+    return response("Board deleted", 200)->header('Content-Type', 'text/plain');
 });
 
 Route::get('/stickies', function(Request $request){
     return Sticky::all();
 });
 
-Route::get('/stickies/add', function(Request $request){
-    return "Add sticky";
+Route::post('/stickies', function(Request $request){
+    $validateFormData = $request->validate([
+        'sticky_content' => 'required|max:500'
+    ]);
+
+    $sticky = new Sticky();
+    $sticky->sticky_type = $request->input('sticky_type');
+    $sticky->bid = $request->input('bid');
+    $sticky->sticky_content = $request->input('sticky_content');
+    $sticky->save();
+    return response("New sticky added", 201)->header('Content-Type', 'text/plain');
 });
 
-Route::get('/stickies/delete/{sid}', function(Request $request, $sid){
-    return "Delete sticky: $sid";
+Route::delete('/stickies', function(Request $request){
+    $sticky_id = $request->input('sticky_id');
+    Sticky::where('sticky_id', $sticky_id)->delete();
+    return response("Sticky deleted", 200)->header('Content-Type', 'text/plain');
 });
 
 Route::get('/groups', function(Request $request){
     return Group::all();
 });
 
-Route::get('/groups/add', function(Request $request){
-    return "Add group";
+Route::post('/groups', function(Request $request){
+    $group = new Group();
+    $group->group_name = $request->input('group_name');
+    $group->board_id = $request->input('bid');
+    $group->sticky_type = $request->input('sticky_type');
+    $group->save();
+    return response("New group added", 201)->header('Content-Type', 'text/plain');
 });
 
-Route::get('/groups/delete/{gid}', function(Request $request, $gid){
-    return "Delete group: $gid";
+Route::delete('/groups', function(Request $request){
+    $group_id = $request->input('group_id');
+    Sticky::where('group_id', $group_id)->delete();
+    Group::where('group_id', $group_id)->delete();
+    return response("Group deleted", 200)->header('Content-Type', 'text/plain');
 });
