f06af7e03eb27dd84ba30d3129fbf7d4b1227e30
Gulyas Gergely <gulyas.gergely@hotmail.com>
2020-12-11 18:43:47 +0100

    RB-60: Moved funcionality into ApiController

--- routes/api.php
+++ routes/api.php
@@ -20,71 +20,12 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/boards', function(Request $request){
-    return Board::all();
-});
-
-Route::post('/boards', function(Request $request){
-    $validateFormData = $request->validate([
-        'board_name' => 'required|max:60',
-        'board_password' => 'max:60'
-    ]);
-
-    $board = new Board();
-    $board->board_name = $request->input('board_name');
-    $board->secure = ($request->input('secure_board') == "on" ? 1 : 0);
-    $board->board_password = Hash::make($request->input('board_password'));
-    $board->save();
-    return response("New board added", 201)->header('Content-Type', 'text/plain');
-});
-
-Route::delete('/boards', function(Request $request){
-    $bid = $request->input('bid');
-    Sticky::where('bid', $bid)->delete();
-    Group::where('board_id', $bid)->delete();
-    Board::where('board_id', $bid)->delete();
-    return response("Board deleted", 200)->header('Content-Type', 'text/plain');
-});
-
-Route::get('/stickies', function(Request $request){
-    return Sticky::all();
-});
-
-Route::post('/stickies', function(Request $request){
-    $validateFormData = $request->validate([
-        'sticky_content' => 'required|max:500'
-    ]);
-
-    $sticky = new Sticky();
-    $sticky->sticky_type = $request->input('sticky_type');
-    $sticky->bid = $request->input('bid');
-    $sticky->sticky_content = $request->input('sticky_content');
-    $sticky->save();
-    return response("New sticky added", 201)->header('Content-Type', 'text/plain');
-});
-
-Route::delete('/stickies', function(Request $request){
-    $sticky_id = $request->input('sticky_id');
-    Sticky::where('sticky_id', $sticky_id)->delete();
-    return response("Sticky deleted", 200)->header('Content-Type', 'text/plain');
-});
-
-Route::get('/groups', function(Request $request){
-    return Group::all();
-});
-
-Route::post('/groups', function(Request $request){
-    $group = new Group();
-    $group->group_name = $request->input('group_name');
-    $group->board_id = $request->input('bid');
-    $group->sticky_type = $request->input('sticky_type');
-    $group->save();
-    return response("New group added", 201)->header('Content-Type', 'text/plain');
-});
-
-Route::delete('/groups', function(Request $request){
-    $group_id = $request->input('group_id');
-    Sticky::where('group_id', $group_id)->delete();
-    Group::where('group_id', $group_id)->delete();
-    return response("Group deleted", 200)->header('Content-Type', 'text/plain');
-});
+Route::get('/boards', 'Api\ApiController@getBoards');
+Route::post('/boards', 'Api\ApiController@addBoard');
+Route::delete('/boards', 'Api\ApiController@deleteBoard');
+Route::get('/stickies', 'Api\ApiController@getStickies');
+Route::post('/stickies', 'Api\ApiController@addSticky');
+Route::delete('/stickies', 'Api\ApiController@deleteSticky');
+Route::get('/groups', 'Api\ApiController@getGroups');
+Route::post('/groups', 'Api\ApiController@addGroup');
+Route::delete('/groups', 'Api\ApiController@deleteGroup');
