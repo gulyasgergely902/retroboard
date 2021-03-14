520fc9e0110db9e948450aa68c808254b81aad88
Gulyas Gergely <gulyas.gergely@hotmail.com>
2021-03-14 20:00:30 +0100

    RB-62: Redesigned display page

--- resources/views/display.blade.php
+++ resources/views/display.blade.php
@@ -2,23 +2,6 @@
 
 @section ('content')
 
-<nav class="navbar navbar-expand-sm fixed-top navbar-light bg-light">
-    @if($protected)
-        <form class="form-inline my-2 mr-2 my-lg-0" method="GET" action="/lock">
-            <input type="hidden" name="bid" value="{{ $bid }}">
-            <button type="submit" class="btn btn-outline-warning btn-lock-board width-2" id="lockboard" title="Lock board"></button>
-        </form>
-    @endif
-    <ul class="nav">
-        <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle text-secondary" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false"><i class="fas fa-ellipsis-v"></i></a>
-            <div class="dropdown-menu dropdown-menu-lg-right">
-                <button type="button" class="btn btn-link text-danger" id="deleteall" title="Clear board" data-toggle="modal" data-target="#clearAllStickyModal" data-bid="{{ $bid }}"><i class="fas fa-quidditch mr-2"></i>Clear board</button>
-            </div>
-        </li>
-    </ul>
-</nav>
-
 <!-- Add Modal -->
 <div class="modal fade" id="addStickyModal" tabindex="-1" role="dialog" aria-labelledby="addModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
@@ -53,9 +36,6 @@
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="clearAllModalLabel">Confirm</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
             </div>
             <div class="modal-body">
                 This will clear the board including the stickies and the groups you've created!
@@ -109,15 +89,17 @@
 
 <!-- Header buttons -->
 <div class="header-buttons px-5 pt-5">
-    <a class="rounded-button {{ Request::is('display/*/0') ? 'active note0' : 'text-dark'}}" id="nav-wentwell-tab" href="/display/{{$bid}}/0" role="tab" aria-controls="nav-wentwell" aria-selected="true" title="Went Well"><i class="fas fa-thumbs-up"></i></a>
-    <a class="rounded-button {{ Request::is('display/*/2') ? 'active note2 ' : 'text-dark'}}" id="nav-needsimprovement-tab" href="/display/{{$bid}}/2" role="tab" aria-controls="nav-needsimprovement" aria-selected="false" title="Needs Improvement"><i class="fas fa-thumbs-down"></i></a>
-    <a class="rounded-button {{ Request::is('display/*/1') ? 'active note1 ' : 'text-dark'}}" id="nav-actionitem-tab" href="/display/{{$bid}}/1" role="tab" aria-controls="nav-actionitem" aria-selected="false" title="Action Item"><i class="fas fa-exclamation"></i></a>
-    <form class="form-toggle-visibility my-2 mr-2 my-lg-0">
+    <a class="rounded-button {{ Request::is('display/*/0') ? 'active note0' : 'text-dark'}}" id="nav-wentwell-tab" href="/display/{{$bid}}/0" aria-controls="nav-wentwell" aria-selected="true" title="Went Well"><i class="fas fa-thumbs-up"></i></a>
+    <a class="rounded-button {{ Request::is('display/*/2') ? 'active note2 ' : 'text-dark'}}" id="nav-needsimprovement-tab" href="/display/{{$bid}}/2" aria-controls="nav-needsimprovement" aria-selected="false" title="Needs Improvement"><i class="fas fa-thumbs-down"></i></a>
+    <a class="rounded-button {{ Request::is('display/*/1') ? 'active note1 ' : 'text-dark'}}" id="nav-actionitem-tab" href="/display/{{$bid}}/1" aria-controls="nav-actionitem" aria-selected="false" title="Action Item"><i class="fas fa-exclamation"></i></a>
+    <p class="header-spacer">|</p>
+    <form class="form-toggle-visibility my-lg-0">
         <label for="hideStickyDiv" class="switch">
             <input class="ml-2" type="checkbox" name="hideStickyDiv" id="hideStickyDiv" title="Toggle sticky visibility" {{ old('hideStickyDiv') }}>
             <span class="slider round"></span>
         </label>
     </form>
+    <button type="button" class="btn btn-link rounded-button danger-button" id="deleteall" title="Clear board" data-toggle="modal" data-target="#clearAllStickyModal" data-bid="{{ $bid }}"><span><i class="fas fa-quidditch mr-2"></i></span></button>
 </div>
 
 <!-- Stickies list -->
@@ -125,9 +107,18 @@
     <div class="card-columns">
         @foreach ($stickies as $sticky)
             <div class="note-base note{{ $sticky->sticky_type }} mr-3 my-3" id="note-base">
-                {{ $sticky->sticky_content }}
-                <div class="btn-group" role="group" aria-label="Sticky actions">
-                    <button type="button" id="new-group" class="btn btn-outline-light btn-sm" title="Delete"><i class="fas fa-trash-alt"></i></button>
+                <div class="upper-shadow hover-display"></div>
+                <div class="delete-sticky-form hover-display">
+                    <form action="{{ url('remove/') }}" method="POST">
+                        <input type="hidden" name="_token" id="token" value="{{ csrf_token() }}">
+                        <input type="hidden" name="mode" value="singlesticky">
+                        <input type="hidden" name="sticky_id" value="{{ $sticky->sticky_id }}">
+                        <input type="hidden" name="bid" value="{{ $bid }}">
+                        <button type="submit" id="new-group" class="btn btn-light btn-sm delete-sticky-button" title="Delete"><i class="fas fa-trash-alt"></i></button>
+                    </form>
+                </div>
+                <div class="note-base-content">
+                    {{ $sticky->sticky_content }}
                 </div>
             </div>
         @endforeach
