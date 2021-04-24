ffe34f4a842f0c99db49a227c6ce10bd6e8b70e2
Gulyas Gergely <gulyas.gergely@hotmail.com>
2021-04-24 16:04:18 +0200

    RB-75: Add dark mode

--- resources/frontend/app/src/App.vue
+++ resources/frontend/app/src/App.vue
@@ -1,12 +1,15 @@
 <template>
     <div id="app">
-        <div id="nav">
-            <p class="top-logo mx-3 my-3"><a class="top-logo-a" @click="goHome()"><img class="header-img" src="./assets/web-logo.png"></a></p>
+        <div id="app-wrapper" :class="(theme === 'darkMode') ? 'dark' : 'light'">
+            <div id="nav">
+                <p class="top-logo mx-3 my-3"><a class="top-logo-a" @click="goHome()"><img class="header-img" v-if="theme === 'darkMode'" src="./assets/web-logo-light.png"><img class="header-img"  v-else src="./assets/web-logo.png"></a></p>
+                <button @click="toggleTheme"><span>Change theme</span></button>
+            </div>
+            <transition name="fade" mode="out-in">
+                <router-view />
+            </transition>
+        <a class="nav-link bottom pill-bg" :class="(theme === 'darkMode') ? 'pill-dark' : 'pill-light'" href="https://github.com/gulyasgergely902/retroboard/issues">Report bugs on GitHub</a>
         </div>
-        <transition name="fade" mode="out-in">
-            <router-view />
-        </transition>
-        <a class="nav-link bottom pill-bg" href="https://github.com/gulyasgergely902/retroboard/issues">Report bugs on GitHub</a>
     </div>
 </template>
 
@@ -16,10 +19,17 @@
 
     @Component
     export default class App extends Vue {
+        theme = '';
+
         goHome() {
             this.$router.push({
                 name: "Index"
             });
+        }
+
+        toggleTheme() {
+            this.theme = this.theme == 'darkMode' ? 'lightMode' : 'darkMode';
+            console.log("theme:" + this.theme);
         }
     }
 </script>
