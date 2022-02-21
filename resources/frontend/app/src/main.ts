aa4c316241127fd1d8c4ce0a991b8bbf3f5132c6
Gulyas Gergely <gulyas.gergely@hotmail.com>
2022-02-21 17:22:07 +0100

    RB-0: Fix icon typecasting

--- resources/frontend/app/src/main.ts
+++ resources/frontend/app/src/main.ts
@@ -11,11 +11,22 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 Vue.use(BootstrapVue)
 
 //FontAwesome
-import { library } from '@fortawesome/fontawesome-svg-core'
+import { IconDefinition, library } from '@fortawesome/fontawesome-svg-core'
 import { faThumbsUp, faThumbsDown, faExclamation, faEye, faEyeSlash, faTrashAlt, faBars, faCheckCircle, faDolly, faLink, faLayerGroup, faPlus } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faThumbsUp, faThumbsDown, faExclamation, faEye, faEyeSlash, faTrashAlt, faBars, faCheckCircle, faDolly, faLink, faLayerGroup, faPlus)
+library.add(faThumbsUp as IconDefinition)
+library.add(faThumbsDown as IconDefinition)
+library.add(faExclamation as IconDefinition)
+library.add(faEye as IconDefinition)
+library.add(faEyeSlash as IconDefinition)
+library.add(faTrashAlt as IconDefinition)
+library.add(faBars as IconDefinition)
+library.add(faCheckCircle as IconDefinition)
+library.add(faDolly as IconDefinition)
+library.add(faLink as IconDefinition)
+library.add(faLayerGroup as IconDefinition)
+library.add(faPlus as IconDefinition)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 //Vue Masonry Layout
